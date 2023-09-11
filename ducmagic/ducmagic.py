8e1d9e446af34776172a15299492b0de833375c1
Willem Jan Faber <willemjan.faber@kb.nl>
2023-09-11 14:53:21 +0200

    no logs on disk.

--- ducmagic/ducmagic.py
+++ ducmagic/ducmagic.py
@@ -13,9 +13,9 @@ import subprocess
 import sys
 import time
 
-file_handler = logging.FileHandler(filename="tmp.log")
+# file_handler = logging.FileHandler(filename="tmp.log")
 stdout_handler = logging.StreamHandler(stream=sys.stdout)
-handlers = [file_handler, stdout_handler]
+handlers = [stdout_handler]
 
 logging.basicConfig(
     level=logging.DEBUG,
