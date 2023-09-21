d065359fc79a036e159c2b9516e1a3c00b785522
Willem Jan Faber <willemjan.faber@kb.nl>
2023-09-21 13:09:20 +0200

    Added speed info. Start on doc, mkdocs or Spyhnxix

--- test_speed.sh
+++ test_speed.sh
@@ -1,3 +1,19 @@
-duc index .
-ducmagic index .
-ducmagic ls
+rm ~/.duc.db; rm ~/.duc_magic.db
+
+echo "Initial"
+time (duc index .; ducmagic index .; ducmagic ls) > /dev/null
+echo
+echo "Cache"
+time (ducmagic ls) > /dev/null
+
+#Initial
+
+#real	0m0,150s
+#user	0m0,208s
+#sys	0m0,110s
+
+#Cache
+
+#real	0m0,051s
+#user	0m0,043s
+#sys	0m0,008s
--- test_speed_bash.sh
+++ test_speed_bash.sh
@@ -1,10 +1,17 @@
-for f in $(find .);do
-    r=$(file "$f") 
-    ft="$(echo $r | cut -d ":" -f 2)" 
-    if [ "$ft" != " directory" ]; then 
-        s=$(ls -s "$f"); 
-        echo "$ft" "$s"
-    else 
-        echo "$ft" "$f" "0"
-    fi
-done
+echo "Initial run"
+time (find . -type d && find . -type f -exec file '{}' ';'; find . -type f -exec ls -s '{}' ';') > /dev/null
+echo 
+echo "Second run"
+time (find . -type d && find . -type f -exec file '{}' ';'; find . -type f -exec ls -s '{}' ';') > /dev/null
+
+#Initial run
+
+#real	0m0,453s
+#user	0m0,359s
+#sys	0m0,095s
+
+#Second run
+
+#real	0m0,454s
+#user	0m0,333s
+#sys	0m0,121s
