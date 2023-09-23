a65926b2c1d7573b81a5fad7c44355e8bc85a437
Willem Jan Faber <willemjan.faber@kb.nl>
2023-09-23 14:44:52 +0200

    Test.

--- test_speed.sh
+++ test_speed.sh
@@ -1,6 +1,17 @@
 echo "Initial"
-/usr/bin/time -o time (duc index / > /dev/null; ducmagic index / > /dev/null; ducmagic ls / > /dev/null)
-(echo "Initial time to index with DUC and ducmagic"; cat time) | cowsay
+export TIME="\t%E,\t%k"
+echo "" > time
+echo "" > time
+/usr/bin/time -a -o time duc index / > /dev/null
+echo "" >> time
+/usr/bin/time -a -o time ducmagic index / > /dev/null
+echo "" >> time
+/usr/bin/time -a -o time ducmagic ls / > /dev/null
+(echo "Initial time to index with DUC and ducmagic"; cat time) | cowsay -f /usr/share/cowsay/cows/ren.cow
 echo "Cache"
-/usr/bin/time -o time (ducmagic ls / > /dev/null)
-(echo "Cached ducmagic call."; cat time) | cowsay
+/usr/bin/time -o time ducmagic ls / > /dev/null
+(echo "Cached ducmagic call."; cat time) | cowsay -f /usr/share/cowsay/cows/ren.cow
+
+
+time find / -exec file '{}' ';'
+time ducmagic ls /
