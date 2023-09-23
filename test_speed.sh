f43526d8ca0dd2bb91119f33d5358fe3a00a27f1
Willem Jan Faber <willemjan.faber@kb.nl>
2023-09-23 12:51:40 +0200

    Add speed compare.

--- test_speed.sh
+++ test_speed.sh
@@ -1,19 +1,6 @@
-rm ~/.duc.db; rm ~/.duc_magic.db
-
 echo "Initial"
-time (duc index .; ducmagic index .; ducmagic ls) > /dev/null
+time (duc index /; ducmagic index /; ducmagic ls /)
 echo
+
 echo "Cache"
-time (ducmagic ls) > /dev/null
-
-#Initial
-
-#real	0m0,150s
-#user	0m0,208s
-#sys	0m0,110s
-
-#Cache
-
-#real	0m0,051s
-#user	0m0,043s
-#sys	0m0,008s
+time (ducmagic ls)
--- test_speed_bash.sh
+++ test_speed_bash.sh
@@ -1,17 +1,5 @@
 echo "Initial run"
-time (find . -type d && find . -type f -exec file '{}' ';'; find . -type f -exec ls -s '{}' ';') > /dev/null
+time (find . -type d && find . -type f -exec file '{}' ';'; find . -type f -exec ls -s '{}' ';')
 echo 
 echo "Second run"
-time (find . -type d && find . -type f -exec file '{}' ';'; find . -type f -exec ls -s '{}' ';') > /dev/null
-
-#Initial run
-
-#real	0m0,453s
-#user	0m0,359s
-#sys	0m0,095s
-
-#Second run
-
-#real	0m0,454s
-#user	0m0,333s
-#sys	0m0,121s
+time (find . -type d && find . -type f -exec file '{}' ';'; find . -type f -exec ls -s '{}' ';')
